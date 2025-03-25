8aae13b69162d7e5b186bed38a1eec47b5e293f6
hrishabhn <hrishabhn@outlook.com>
2025-03-25 13:25:52 +0100

    Add dev dependencies

---
+++ places-react/.prettierrc.js
@@ -0,0 +1,34 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: false,
+    printWidth: 180,
+    arrowParens: 'avoid',
+    plugins: [
+        //
+        '@trivago/prettier-plugin-sort-imports',
+        'prettier-plugin-tailwindcss',
+    ],
+
+    tailwindFunctions: ['tw'],
+
+    importOrder: [
+        //
+        '^server-only$',
+        '^[./]',
+        '<THIRD_PARTY_MODULES>',
+        '^@/model/(.*)$',
+        '^@/lib/(.*)$',
+        '^@/components/(.*)$',
+    ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+}
+
+module.exports = config
